d5935864e389840eb689e22e199bd32ebf9ed0f2
SharpDerek <dsharp640@comcast.net>
2021-06-22 09:34:19 -0600

    Update uxi-woo360-migrator.php

--- uxi-woo360-migrator.php
+++ uxi-woo360-migrator.php
@@ -139,13 +139,14 @@ register_uninstall_hook(__FILE__, 'uxi_migration_uninstall');
 function uxi_migration_uninstall() {
 	$uxi_files_dir = trailingslashit(WP_CONTENT_DIR) . 'uploads/uxi-files';
 
-	$directory_iterator = new RecursiveDirectoryIterator($uxi_files_dir, FilesystemIterator::SKIP_DOTS);
-	$recursive_iterator = new RecursiveIteratorIterator($directory_iterator, RecursiveIteratorIterator::CHILD_FIRST);
+	if (file_exists($uxi_files_dir)) {
+		$directory_iterator = new RecursiveDirectoryIterator($uxi_files_dir, FilesystemIterator::SKIP_DOTS);
+		$recursive_iterator = new RecursiveIteratorIterator($directory_iterator, RecursiveIteratorIterator::CHILD_FIRST);
 
-	foreach($recursive_iterator as $file) {
-		$file->isDir() ? rmdir($file) : unlink($file);
+		foreach($recursive_iterator as $file) {
+			$file->isDir() ? rmdir($file) : unlink($file);
+		}
 	}
-	rmdir($uxi_files_dir);
 
 	delete_option('uxi_migrator_site_url');
 }
